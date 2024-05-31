f6a9549248e2708624a7005d1ac7fcc51982c99b
natsukium <tomoya.otabi@gmail.com>
2024-05-31 10:21:36 +0900

    refactor

---
+++ src/pytest_sandbox/plugin.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import http.client
+from typing import Any
+
+import pytest
+
+
+def pytest_addoption(parser: pytest.Parser):
+    group = parser.getgroup("sandbox", "sandbox test helper")
+    group.addoption("--allow-network", action="store_true", help="Allow network access", default=False)
+
+
+def xfail(*args: Any, **kwargs: Any):
+    pytest.xfail("try to access network")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def xfail_http(pytestconfig: pytest.Config):
+    if pytestconfig.getoption("--allow-network"):
+        yield
+
+    else:
+        monkeypatch = pytest.MonkeyPatch()
+        monkeypatch.setattr(http.client.HTTPConnection, "__init__", xfail)
+
+        yield
+
+        monkeypatch.undo()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def xfail_aiohttp(pytestconfig: pytest.Config):
+    if pytestconfig.getoption("--allow-network"):
+        yield
+    else:
+        try:
+            import aiohttp
+
+            monkeypatch = pytest.MonkeyPatch()
+            monkeypatch.setattr(aiohttp.ClientSession, "__init__", xfail)
+
+            yield
+
+            monkeypatch.undo()
+
+        except ImportError:
+            yield
+
+
+@pytest.fixture(scope="session", autouse=True)
+def xfail_httpcore(pytestconfig: pytest.Config):
+    if pytestconfig.getoption("--allow-network"):
+        yield
+    else:
+        try:
+            import httpcore
+
+            monkeypatch = pytest.MonkeyPatch()
+            monkeypatch.setattr(httpcore.Request, "__init__", xfail)
+
+            yield
+
+            monkeypatch.undo()
+
+        except ImportError:
+            yield
--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -15,44 +15,28 @@ def test_urllib(url: str):
 
 
 def test_request(url: str):
-    try:
-        import requests
-    except ImportError:
-        pytest.skip("requests is not installed")
-
+    requests = pytest.importorskip("requests")
     requests.get(url)
     raise RuntimeError("Network access is prohibited in the sandbox")
 
 
 @pytest.mark.asyncio
 async def test_aiohttp(url: str):
-    try:
-        import aiohttp
-    except ImportError:
-        pytest.skip("aiohttp is not installed")
-
+    aiohttp = pytest.importorskip("aiohttp")
     async with aiohttp.ClientSession() as session:
         await session.get(url)
     raise RuntimeError("Network access is prohibited in the sandbox")
 
 
 def test_httpx(url: str):
-    try:
-        import httpx
-    except ImportError:
-        pytest.skip("httpx is not installed")
-
+    httpx = pytest.importorskip("httpx")
     httpx.get(url)
     raise RuntimeError("Network access is prohibited in the sandbox")
 
 
 @pytest.mark.asyncio
 async def test_httpx_async(url: str):
-    try:
-        import httpx
-    except ImportError:
-        pytest.skip("httpx is not installed")
-
+    httpx = pytest.importorskip("httpx")
     async with httpx.AsyncClient() as client:
         await client.get(url)
     raise RuntimeError("Network access is prohibited in the sandbox")
